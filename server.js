25928e3551c376ad8ab759bc9bd150732dc20aaa
vinaykangralkar <vinaykangralkar9@gmail.com>
2020-12-25 10:07:18 +1100

    Updates

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const signin = require('./constrollers/signin');
 const profile = require('./constrollers/profile');
 const image = require('./constrollers/image');
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
-
 const db = knex({
     client: 'pg',
     connection: {
       connectionString : process.env.DATABASE_URL, 
-      ssl: true
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
 });
 
