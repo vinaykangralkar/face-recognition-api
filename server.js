ffc8722913d1b3f7e93987022c3cc38788cee547
vinaykangralkar <vinaykangralkar9@gmail.com>
2020-12-27 19:46:38 +1100

    Updates

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'vinay',
+    user : 'vinaykangralkar',
     password : '',
     database : 'face-recognition'
   }
